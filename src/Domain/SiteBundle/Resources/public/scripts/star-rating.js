0273d82ae322d67d552ada288f4cabb323d626a1
George Kovrey <george.kovrey@oxagile.com>
2016-06-01 19:17:42 +0300

    feature(site-bundle): addting styles and assets

---
+++ src/Domain/SiteBundle/Resources/public/scripts/star-rating.js
@@ -0,0 +1,30 @@
+var rating = function() {
+};
+
+rating.prototype.starsRating = function() {
+    var starRating = $( '.star-rating .fa' );
+
+    starRating.on('click', function(event) {
+        $( event.currentTarget ).parent().addClass( 'active' );
+        $( event.currentTarget ).siblings( 'input.rating-value' ).val( $( this ).data( 'rating' ));
+
+        return starRating.each(function() {
+            if( $( this ).parent().hasClass( 'active' ) ){
+                if (parseInt( $(this).siblings( 'input.rating-value' ).val()) >= parseInt( $( this ).data( 'rating' ))) {
+                    return $( this ).removeClass( 'fa-star' ).addClass( 'fa-star-selected' );
+                } else {
+                    return $( this ).removeClass( 'fa-star-selected' ).addClass( 'fa-star' );
+                }
+            }
+       });
+    });
+};
+
+rating.prototype.run = function() {
+    this.starsRating();
+};
+
+$( function () {
+    var controller = new rating();
+    controller.run();
+});
