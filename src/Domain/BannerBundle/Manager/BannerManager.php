ffa9688dc64a6874f2f06d0e96fe3243902fccaf
George Kovrey <george.kovrey@oxagile.com>
2016-06-09 16:32:22 +0300

    feature(homepage-footer): adding social links from config entity

--- src/Domain/BannerBundle/Manager/BannerManager.php
+++ src/Domain/BannerBundle/Manager/BannerManager.php
@@ -4,16 +4,37 @@ namespace Domain\BannerBundle\Manager;
 
 use Doctrine\ORM\EntityManager;
 
+/**
+ * Class BannerManager
+ * Banner management entry point
+ *
+ * @package Domain\BannerBundle\Manager
+ */
 class BannerManager
 {
+    /**
+     * @var EntityManager
+     */
     protected $em;
 
+    /**
+     * BannerManager constructor.
+     *
+     * @access public
+     * @param EntityManager $entityManager
+     */
     public function __construct(EntityManager $entityManager)
     {
         $this->em = $entityManager;
     }
 
-    public function getBanner($bannerType)
+    /**
+     * factory-like methid for retrieving banners for different places
+     *
+     * @param int $bannerType
+     * @return string|object
+     */
+    public function getBanner(int $bannerType)
     {
         return '<img src="/bundles/domainsite/images/banner.png">';
     }
--- src/Domain/SiteBundle/Controller/HomeController.php
+++ src/Domain/SiteBundle/Controller/HomeController.php
@@ -26,12 +26,14 @@ class HomeController extends Controller
      */
     public function homeAction()
     {
-        $searchManager = $this->get('domain_site.manager.search');
-        $bannerManager = $this->get('domain_banner.manager.banner');
+        $searchManager  = $this->get('domain_site.manager.search');
+        $bannerManager  = $this->get('domain_banner.manager.banner');
+        $artcileManager = $this->get('domain_site.manager.search');
+
 
         $searchMenuList = $searchManager->getQuickSearchMenuLinksList();
         $banner         = $bannerManager->getBanner(TypeInterface::CODE_HOME);
-        
+
         return $this->render(
             'DomainSiteBundle:Home:home.html.twig',
             array(
