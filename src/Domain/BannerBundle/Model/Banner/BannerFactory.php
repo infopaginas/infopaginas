e9f22e67bb98e34ca4162143e11c2156d706848f
George Kovrey <george.kovrey@oxagile.com>
2016-06-13 20:30:31 +0300

    feature(banners): factory and strategy

--- src/Domain/BannerBundle/Model/Banner/BannerFactory.php
+++ src/Domain/BannerBundle/Model/Banner/BannerFactory.php
@@ -3,8 +3,37 @@
 namespace Domain\BannerBundle\Model\Banner\Factory;
 
 use Oxa\ManagerArchitectureBundle\Model\Factory\Factory;
+use Domain\BannerBundle\Model\TypeInterface as BannerType;
 
 class BannerFactory extends Factory
 {
-    
+    public function get($type)
+    {
+        $banner = null;
+        switch ($type) {
+            case BannerType::CODE_HOME:
+                $this->getHomepageBanner();
+                break;
+            case BannerType::CODE_PORTAL:
+                break;
+            case BannerType::CODE_PORTAL_LEADERBOARD:
+                break;
+            case BannerType::CODE_SERP_BANNER:
+                break;
+            case BannerType::CODE_SERP_BOXED:
+                break;
+            case BannerType::CODE_SERP_FEATUREAD:
+                break;
+            case BannerType::CODE_SERP_MOBILE_TOP:
+                break;
+            default:
+                // TODO: proper exception type/message
+                throw new Exception('Undefined type');
+        }
+    }
+
+    protected function getHomepageBanner()
+    {
+        
+    }
 }
--- src/Oxa/ManagerArchitectureBundle/Model/Interfaces/ServiceInterface.php
+++ src/Oxa/ManagerArchitectureBundle/Model/Interfaces/ServiceInterface.php
@@ -2,6 +2,8 @@
 
 namespace Oxa\ManagerArchitectureBundle\Model\Interfaces;
 
+use Doctrine\ORM\EntityManager;
+
 interface ServiceInterface
 {
     /**
