fefbd13fe7a1ffc28484152a9b81b2de0b942fed
George Kovrey <george.kovrey@oxagile.com>
2016-06-17 19:36:50 +0300

    hotfix(banners-and-images): fix of position and sizes, also changing links to articles

--- src/Domain/ArticleBundle/Controller/DefaultController.php
+++ src/Domain/ArticleBundle/Controller/DefaultController.php
@@ -6,4 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+    public function indexAction()
+    {
+        return $this->render('DomainSiteBundle:Home:search.html.twig');
+    }
+
+    public function viewAction()
+    {
+        return $this->render('DomainSiteBundle:Home:search.html.twig');
+    }
 }
