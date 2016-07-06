0cacb7d41436c26c5b6bc55948faee0c772549a8
Polevoy, Alexander <alexander.polevoy@oxagile.com>
2016-07-06 20:20:56 +0300

    fix(IPGN-172): Returned try-catch back to BusinessProfileController.

--- src/Domain/BusinessBundle/Controller/ProfileController.php
+++ src/Domain/BusinessBundle/Controller/ProfileController.php
@@ -61,8 +61,12 @@ class ProfileController extends Controller
     {
         $formHandler = $this->getFreeBusinessProfileFormHandler();
 
-        if ($formHandler->process()) {
-            return $this->getSuccessResponse(self::SUCCESS_PROFILE_REQUEST_CREATED_MESSAGE);
+        try {
+            if ($formHandler->process()) {
+                return $this->getSuccessResponse(self::SUCCESS_PROFILE_REQUEST_CREATED_MESSAGE);
+            }
+        } catch (\Exception $e) {
+            return $this->getFailureResponse($e->getMessage(), [], self::INTERNAL_SERVER_ERROR_STATUS_CODE);
         }
 
         return $this->getFailureResponse(self::ERROR_VALIDATION_FAILURE, $formHandler->getErrors());
