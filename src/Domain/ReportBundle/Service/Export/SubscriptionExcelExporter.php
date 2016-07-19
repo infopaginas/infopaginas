96ce77fca047e7592155232c5547b438a3abc44a
Karpovich Aleksandr <aleksandr.karpovich@oxagile.com>
2016-07-19 12:59:22 +0300

    fix(export): add roles and fix excel export

--- src/Domain/ReportBundle/Service/Export/SubscriptionExcelExporter.php
+++ src/Domain/ReportBundle/Service/Export/SubscriptionExcelExporter.php
@@ -148,7 +148,6 @@ class SubscriptionExcelExporter extends ExcelExporterModel
         );
 
         // for each subscription plan
-
         foreach ($subscriptionData['subscription_quantities'] as $subscription) {
             ++$row;
             $activeSheet->setCellValue(
