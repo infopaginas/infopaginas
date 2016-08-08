8f05d8bd938e72b268686914915cedaf3cda8645
Aleksandr Sakov <BELHARD\sakovas@debian-8.2>
2016-08-08 15:38:19 +0300

    fix(IPGN-755): unique spain name of area fix

--- src/Domain/BusinessBundle/Entity/Translation/AreaTranslation.php
+++ src/Domain/BusinessBundle/Entity/Translation/AreaTranslation.php
@@ -3,29 +3,21 @@
 namespace Domain\BusinessBundle\Entity\Translation;
 
 use Doctrine\ORM\Mapping as ORM;
-use Oxa\Sonata\AdminBundle\Model\CopyableEntityInterface;
 use Sonata\TranslationBundle\Model\Gedmo\AbstractPersonalTranslation;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="area_translation",
  *     uniqueConstraints={@ORM\UniqueConstraint(name="lookup_unique_area_translation_idx", columns={
- *         "locale", "object_id", "field"
+ *         "locale", "field", "content"
  *     })}
  * )
- * @UniqueEntity("name")
  */
-class AreaTranslation extends AbstractPersonalTranslation implements CopyableEntityInterface
+class AreaTranslation extends AbstractPersonalTranslation
 {
     /**
      * @ORM\ManyToOne(targetEntity="Domain\BusinessBundle\Entity\Area", inversedBy="translations")
      * @ORM\JoinColumn(name="object_id", referencedColumnName="id", onDelete="CASCADE")
      */
     protected $object;
-
-    public function getMarkCopyPropertyName()
-    {
-        return 'name';
-    }
 }
