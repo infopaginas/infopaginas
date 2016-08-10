2e7b0432f1a1173b5c62b41c14dfeeb34637405d
George Kovrey <george.kovrey@oxagile.com>
2016-08-10 17:31:06 +0300

    fix(requirejs): fix of loading modules

--- src/Domain/BusinessBundle/Resources/public/scripts/modules/businessProfile.js
+++ src/Domain/BusinessBundle/Resources/public/scripts/modules/businessProfile.js
@@ -1,4 +1,4 @@
-define(['jquery', 'bootstrap', 'alertify', 'tools/form', 'tools/spin', 'tools/select', 'tools/phones'], function( $, bootstrap, alertify, FormHandler, Spin, select ) {
+define(['jquery', 'bootstrap', 'alertify', 'businessTools/form', 'tools/spin', 'businessTools/select', 'businessTools/phones'], function( $, bootstrap, alertify, FormHandler, Spin, select ) {
     'use strict';
 
     //init businessProfile object variables
