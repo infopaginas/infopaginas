ba5a9102cb655938868c89f4ff583019b575a519
Aleksandr Sakov <BELHARD\sakovas@debian-8.2>
2016-08-15 13:19:16 +0300

    fix(IPGN-763): unique email validation when deleted user

--- src/Domain/SiteBundle/Validator/Constraints/ContainsEmailExpandedValidator.php
+++ src/Domain/SiteBundle/Validator/Constraints/ContainsEmailExpandedValidator.php
@@ -2,6 +2,10 @@
 
 namespace Domain\SiteBundle\Validator\Constraints;
 
+use Doctrine\ORM\EntityManager;
+use Oxa\Sonata\UserBundle\Entity\User;
+use Gedmo\SoftDeleteable\Filter\SoftDeleteableFilter;
+use Oxa\Sonata\UserBundle\Manager\UsersManager;
 use Symfony\Component\Translation\TranslatorInterface;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\Constraints\Email;
@@ -10,24 +14,74 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ContainsEmailExpandedValidator extends ConstraintValidator
 {
-    const EMAIL_REGEX_PATTERN = '(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})';
+    const EMAIL_REGEX_PATTERN = '^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$';
 
+    /**
+     * @var TranslatorInterface
+     */
     protected $translator;
+
+    /**
+     * @var ValidatorInterface
+     */
     protected $validator;
 
-    public function __construct(TranslatorInterface $translator, ValidatorInterface $validator)
-    {
-        $this->translator = $translator;
-        $this->validator  = $validator;
+    /**
+     * @var EntityManager
+     */
+    protected $entityManager;
+
+    /**
+     * @var UsersManager
+     */
+    protected $usersManager;
+
+    /**
+     * @param TranslatorInterface $translator
+     * @param ValidatorInterface $validator
+     * @param EntityManager $entityManager
+     * @param UsersManager $usersManager
+     */
+    public function __construct(
+        TranslatorInterface $translator,
+        ValidatorInterface $validator,
+        EntityManager $entityManager,
+        UsersManager $usersManager
+    ) {
+        $this->translator     = $translator;
+        $this->validator      = $validator;
+        $this->entityManager  = $entityManager;
+        $this->usersManager   = $usersManager;
     }
 
+    /**
+     * @param $value
+     * @param Constraint $constraint
+     */
     public function validate($value, Constraint $constraint)
     {
         $isCustomValid = $this->validator->validateValue($value, new Email());
 
-        if (!count($isCustomValid) && !preg_match('/' . self::EMAIL_REGEX_PATTERN . '/', $value)) {
+        if (count($isCustomValid) > 0 && !preg_match('/' . self::EMAIL_REGEX_PATTERN . '/', $value)) {
             $this->context
                 ->buildViolation($this->translator->trans('fos_user.email.invalid'))
+                ->addViolation();
+        }
+
+        /**
+         * @var SoftDeleteableFilter $softDeleteableFilter
+         */
+        $softDeleteableFilter = $this->entityManager
+            ->getFilters()
+            ->getFilter('softdeleteable');
+
+        $softDeleteableFilter->disableForEntity(User::class);
+
+        $user = $this->usersManager->getUserByEmail($value);
+
+        if ($user) {
+            $this->context
+                ->buildViolation($this->translator->trans('fos_user.email.already_used'))
                 ->addViolation();
         }
     }
