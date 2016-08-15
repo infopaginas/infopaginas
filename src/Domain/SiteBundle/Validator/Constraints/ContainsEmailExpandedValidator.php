fa92eaad3b2033719a71a41dfd8701c51ddd5e0b
Aleksandr Sakov <BELHARD\sakovas@debian-8.2>
2016-08-15 14:15:49 +0300

    fix(IPGN-763): move unique validation in separeted validator

--- src/Domain/SiteBundle/Validator/Constraints/ContainsEmailExpandedValidator.php
+++ src/Domain/SiteBundle/Validator/Constraints/ContainsEmailExpandedValidator.php
@@ -2,10 +2,7 @@
 
 namespace Domain\SiteBundle\Validator\Constraints;
 
-use Doctrine\ORM\EntityManager;
-use Oxa\Sonata\UserBundle\Entity\User;
 use Gedmo\SoftDeleteable\Filter\SoftDeleteableFilter;
-use Oxa\Sonata\UserBundle\Manager\UsersManager;
 use Symfony\Component\Translation\TranslatorInterface;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\Constraints\Email;
@@ -27,31 +24,13 @@ class ContainsEmailExpandedValidator extends ConstraintValidator
     protected $validator;
 
     /**
-     * @var EntityManager
-     */
-    protected $entityManager;
-
-    /**
-     * @var UsersManager
-     */
-    protected $usersManager;
-
-    /**
      * @param TranslatorInterface $translator
      * @param ValidatorInterface $validator
-     * @param EntityManager $entityManager
-     * @param UsersManager $usersManager
      */
-    public function __construct(
-        TranslatorInterface $translator,
-        ValidatorInterface $validator,
-        EntityManager $entityManager,
-        UsersManager $usersManager
-    ) {
-        $this->translator     = $translator;
-        $this->validator      = $validator;
-        $this->entityManager  = $entityManager;
-        $this->usersManager   = $usersManager;
+    public function __construct(TranslatorInterface $translator, ValidatorInterface $validator)
+    {
+        $this->translator   = $translator;
+        $this->validator    = $validator;
     }
 
     /**
@@ -65,23 +44,6 @@ class ContainsEmailExpandedValidator extends ConstraintValidator
         if (count($isCustomValid) > 0 && !preg_match('/' . self::EMAIL_REGEX_PATTERN . '/', $value)) {
             $this->context
                 ->buildViolation($this->translator->trans('fos_user.email.invalid'))
-                ->addViolation();
-        }
-
-        /**
-         * @var SoftDeleteableFilter $softDeleteableFilter
-         */
-        $softDeleteableFilter = $this->entityManager
-            ->getFilters()
-            ->getFilter('softdeleteable');
-
-        $softDeleteableFilter->disableForEntity(User::class);
-
-        $user = $this->usersManager->getUserByEmail($value);
-
-        if ($user) {
-            $this->context
-                ->buildViolation($this->translator->trans('fos_user.email.already_used'))
                 ->addViolation();
         }
     }
