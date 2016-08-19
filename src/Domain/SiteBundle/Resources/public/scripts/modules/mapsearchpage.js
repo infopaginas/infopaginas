51427b3e24a6558b13300dd7820a98b2e3dc8381
Sosnovski Artem <sosnovski.artem@oxagile.com>
2016-08-19 13:23:35 +0300

    fixes for header

--- src/Domain/SiteBundle/Resources/public/scripts/modules/mapsearchpage.js
+++ src/Domain/SiteBundle/Resources/public/scripts/modules/mapsearchpage.js
@@ -16,16 +16,16 @@ define(
         this.mapSize = {
             mapWrapper: '#search-results-map',
             mapImage: '#map-canvas',
-            mapTopMargin: 150,
             mapLeftMargin: 515,
-            mapMediaWidth: 992
+            mapMediaWidth: 992,
+            marginTop: 120
         };
 
         this.init( options );
         this.bindEvents();
 
         $( document ).ready( function(){
-            this.resizeMap()
+            this.resizeMap();
         }.bind( this ) );
 
         return this;
@@ -40,7 +40,7 @@ define(
             itemsListScrollable : '.map-view-aside',
             mapContainer : 'map-canvas',
             mapOptions   : {
-                center: new google.maps.LatLng(18.2208, -66.5901),
+                center: new google.maps.LatLng( 18.2208, -66.5901 ),
                 zoom: 8
             },
             cards       : '.card-item',
@@ -62,10 +62,10 @@ define(
             mapWrapperHeight = $( this.mapSize.mapWrapper ).height();
         if( mapWrapperWidth < this.mapSize.mapMediaWidth ){
             $( this.mapSize.mapImage ).width( mapWrapperWidth );
-            $( this.mapSize.mapImage ).height( mapWrapperHeight - this.mapSize.mapTopMargin );
+            $( this.mapSize.mapImage ).height( mapWrapperHeight - this.mapSize.marginTop );
         } else{
             $( this.mapSize.mapImage ).width( mapWrapperWidth - this.mapSize.mapLeftMargin );
-            $( this.mapSize.mapImage ).height( mapWrapperHeight - this.mapSize.mapTopMargin );
+            $( this.mapSize.mapImage ).height( mapWrapperHeight - this.mapSize.marginTop );
         }
     };
 
@@ -81,7 +81,7 @@ define(
         var bounds = new google.maps.LatLngBounds();
 
         _.each(this.markers, function ( markerItem ) {
-            bounds.extend( markerItem.marker.getPosition());
+            bounds.extend( markerItem.marker.getPosition() );
         });
 
         this.map.fitBounds( bounds );
@@ -105,7 +105,7 @@ define(
             labelContent: "Ololoshenki",
             labelInBackground: false,
             labelAnchor: new google.maps.Point(3, 30),
-            labelClass: "labels", // the CSS class for the label
+            labelClass: "labels" // the CSS class for the label
         });
 
         var infoWindow = new google.maps.InfoWindow({
@@ -114,8 +114,8 @@ define(
 
         marker.addListener( 'click', function( event ) {
             self.closeAllLables();
-            self.scrollTo(markerData.id);
-            infoWindow.open(self.map, marker);
+            self.scrollTo( markerData.id );
+            infoWindow.open( self.map, marker );
         });
 
         var markerObjec = {};
@@ -123,48 +123,48 @@ define(
             marker : marker,
             infoWindow : infoWindow
         }
-    }
+    };
 
     mapSearchPage.prototype.scrollTo = function ( elementId )
     {
-        var card = this.$('#' + elementId);
+        var card = this.$( '#' + elementId );
         var offset = card.offset().top;
         this.$( this.options.itemsListScrollable).first()
             .animate({
                 scrollTop : offset
             }, 1500);
         this.highlightCard( elementId ); 
-    }
+    };
 
     mapSearchPage.prototype.showMarker = function ( event )
     {
-        this.highlightMarker($(event.target).parents('.card-item').data('id'));
-    }
+        this.highlightMarker( $( event.target ).parents( '.card-item' ).data( 'id' ));
+    };
 
     mapSearchPage.prototype.highlightMarker = function ( elementId )
     {
         new google.maps.event.trigger( this.markers[elementId].marker, 'click' );
-    }
+    };
 
     mapSearchPage.prototype.highlightCard = function ( elementId )
     {
         this.deHighlightCards();
-        this.$("#" + elementId).addClass('selected-card');
-    }
+        this.$( "#" + elementId ).addClass( 'selected-card' );
+    };
 
     mapSearchPage.prototype.deHighlightCards = function ()
     {
-        this.$('.selected-card').removeClass('selected-card');
-    }
+        this.$( '.selected-card' ).removeClass( 'selected-card' );
+    };
 
     mapSearchPage.prototype.closeAllLables = function ()
     {
         _.each(this.markers, function( item ) {
             item.infoWindow.close()
         });
-    }
+    };
 
-    mapSearchPage.prototype.getInfoHTML = function (name, address, reviewsCount, avgMark, icon)
+    mapSearchPage.prototype.getInfoHTML = function ( name, address, reviewsCount, avgMark, icon )
     {
         var template = "<div class='business-info'>" +
             "<div>" + name + "</div>" +
@@ -189,14 +189,14 @@ define(
             }
 
             return  template;
-    }
+    };
 
-    mapSearchPage.prototype.setDefaultHeighForCards = function (cards)
+    mapSearchPage.prototype.setDefaultHeighForCards = function ( cards )
     {
-        _.each(cards, function (card) {
-            $(card).data('default-offset', $(card).offset().top)
+        _.each( cards, function ( card ) {
+            $(card).data( 'default-offset', $( card ).offset().top )
         })
-    }
+    };
 
     return mapSearchPage;
 });
