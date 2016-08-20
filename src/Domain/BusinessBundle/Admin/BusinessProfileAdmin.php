36c0e4c05d5f9c1cd66414e73a6bbd10d2f175d4
Aleksandr Sakov <BELHARD\sakovas@debian-8.2>
2016-08-20 15:24:19 +0300

    fix(IPGN-903): mark as required profile fields at admin panel

--- src/Domain/BusinessBundle/Admin/BusinessProfileAdmin.php
+++ src/Domain/BusinessBundle/Admin/BusinessProfileAdmin.php
@@ -154,19 +154,19 @@ class BusinessProfileAdmin extends OxaAdmin
                 ->end()
                 ->with('Address')
                     ->add('country', 'sonata_type_model_list', [
-                        'required' => false,
+                        'required' => true,
                         'btn_delete' => false,
                         'btn_add' => false,
                     ])
                     ->add('state')
                     ->add('city', null, [
-                        'required' => false
+                        'required' => true
                     ])
                     ->add('zipCode', null, [
-                        'required' => false
+                        'required' => true
                     ])
                     ->add('streetAddress', null, [
-                        'required' => false
+                        'required' => true
                     ])
                     ->add('extendedAddress')
                     ->add('crossStreet')
@@ -192,8 +192,14 @@ class BusinessProfileAdmin extends OxaAdmin
                     ->add('youtubeURL')
                 ->end()
                 ->with('Categories')
-                    ->add('categories', null, ['multiple' => true])
-                    ->add('areas', null, ['multiple' => true])
+                    ->add('categories', null, [
+                        'multiple' => true,
+                        'required' => true,
+                    ])
+                    ->add('areas', null, [
+                        'multiple' => true,
+                        'required' => true,
+                    ])
                     ->add('brands', null, ['multiple' => true])
                     ->add('tags', null, ['multiple' => true])
                     ->add('paymentMethods', null, [
