cc9014c00ed83d3fb023ed3be2a6674820e17fab
Sosnovski Artem <sosnovski.artem@oxagile.com>
2016-09-01 14:19:56 +0300

    compare page fix

--- src/Domain/SiteBundle/Resources/public/scripts/modules/tools/comparison.js
+++ src/Domain/SiteBundle/Resources/public/scripts/modules/tools/comparison.js
@@ -1,4 +1,4 @@
-define(['jquery', 'tools/slick'], function ($) {
+define(['jquery'], function ( $ ) {
     'use strict';
 
     var comparison = function() {
@@ -9,85 +9,47 @@ define(['jquery', 'tools/slick'], function ($) {
 
     comparison.prototype.heightSetter = function(){
         comparison.prototype.resetHeightSetter();
-        var listOfCardChildren= $(".card-item").first().children();
-        var listOfTitles = $("#compare-block").first().children();
-        listOfCardChildren.each(function(i, el){
+        var listOfCardChildren= $( ".card-item" ).first().children();
+        var listOfTitles = $( "#compare-block" ).first().children();
+        listOfCardChildren.each( function( i, el ){
             var elementClass = el.className.split(' ').join('.');
-            var elements = $('.' + elementClass);
-            var maxHeight = Math.max.apply(null, elements.map(function ()
+            var elements = $( '.' + elementClass );
+            var maxHeight = Math.max.apply( null, elements.map( function ()
             {
-                return $(this).height();
+                return $( this ).height();
             }).get());
-            elements.height(maxHeight);
-            $(listOfTitles[i]).height(maxHeight);
+            elements.height( maxHeight );
+            $( listOfTitles[i] ).height( maxHeight );
         });
     };
 
     comparison.prototype.resetHeightSetter = function(){
-        var listOfCardChildren= $(".card-item").first().children();
-        listOfCardChildren.each(function(i, el){
+        var listOfCardChildren= $( ".card-item" ).first().children();
+        listOfCardChildren.each( function( i, el ){
             var elementClass = el.className.split(' ').join('.');
-            var elements = $('.' + elementClass);
-            elements.each(function(i, el){
+            var elements = $( '.' + elementClass );
+            elements.each( function( i, el ){
                 el.style.height = "";
             })
         });
     };
 
-    // comparison.prototype.comparisonCarousel = function() {
-    //     var $carousel = $( '.carousel-property' ),
-    //         nextButton = $( '.nextSlide' ),
-    //         prevButton = $( '.prevSlide' ),
-    //         sliderWrap = $( '.comparison-wrap'),
-    //         sliderBlock = $( '.comparison-block'),
-    //         cardItem = $( '.card-item'),
-    //         cardItemWidth = 220,
-    //         slickwrap = $( '.slick-track'),
-    //         closeButton = $( '.times' );
-    //
-    //     $carousel.slick({
-    //         slidesToShow: 1,
-    //         slidesToScroll: 1,
-    //         infinite: false,
-    //         dots: false,
-    //         accessibility: false,
-    //         swipe: true,
-    //         prevArrow: $( '.prevSlide' ),
-    //         nextArrow: $( '.nextSlide' ),
-    //     });
-    //
-    //
-    //     sliderWrap.width( cardItemWidth * cardItem.length );
-    //
-    //     closeButton.on( 'click', function() {
-    //         $( this ).closest( '.card-item' ).remove();
-    //         sliderWrap.width( cardItemWidth * ( $( '.card-item').length - 1));
-    //         comparison.prototype.heightSetter();
-    //     });
-    //
-    //     $carousel.on( 'swipe', function(event, slick, direction){
-    //         if(direction == "left") {
-    //             var leftPos = sliderBlock.scrollLeft();
-    //             sliderBlock.animate({ scrollLeft: leftPos + cardItemWidth }, 800 );
-    //         } else {
-    //             var leftPos = sliderBlock.scrollLeft();
-    //             sliderBlock.animate({ scrollLeft: leftPos - cardItemWidth }, 800 );
-    //         }
-    //     });
-    //
-    //     nextButton.on( 'click', function() {
-    //         var leftPos = sliderBlock.scrollLeft();
-    //         sliderBlock.animate({ scrollLeft: leftPos + cardItemWidth }, 800 );
-    //     });
-    //
-    //     prevButton.on( 'click', function() {
-    //         var leftPos = sliderBlock.scrollLeft();
-    //         sliderBlock.animate({ scrollLeft: leftPos - cardItemWidth }, 800 );
-    //     });
-    // }
+    comparison.prototype.comparisonCarousel = function() {
+
+        var sliderWrap = $( '.comparison-wrap' ),
+            cardItemWidth = 220,
+            closeButton = $( '.times' );
+
+        closeButton.on( 'click', function() {
+            $( this ).closest( '.card-item' ).remove();
+            sliderWrap.width( cardItemWidth * ( $( '.card-item' ).length - 1));
+            comparison.prototype.heightSetter();
+        });
+
+    };
 
     comparison.prototype.run = function() {
-        // this.comparisonCarousel();
+        this.comparisonCarousel();
         this.heightSetter();
     };
 
