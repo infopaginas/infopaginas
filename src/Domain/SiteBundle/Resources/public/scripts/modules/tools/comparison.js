8678c31ae671292e53cd01c755d4f7dc28d0f0b7
Sosnovski Artem <sosnovski.artem@oxagile.com>
2016-09-01 11:23:27 +0300

    compare page

--- src/Domain/SiteBundle/Resources/public/scripts/modules/tools/comparison.js
+++ src/Domain/SiteBundle/Resources/public/scripts/modules/tools/comparison.js
@@ -34,60 +34,60 @@ define(['jquery', 'tools/slick'], function ($) {
         });
     };
 
-    comparison.prototype.comparisonCarousel = function() {
-        var $carousel = $( '.carousel-property' ),
-            nextButton = $( '.nextSlide' ),
-            prevButton = $( '.prevSlide' ),
-            sliderWrap = $( '.comparison-wrap'),
-            sliderBlock = $( '.comparison-block'),
-            cardItem = $( '.card-item'),
-            cardItemWidth = 220,
-            slickwrap = $( '.slick-track'),
-            closeButton = $( '.times' );
-
-        $carousel.slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            infinite: false,
-            dots: false,
-            accessibility: false,
-            swipe: true,
-            prevArrow: $( '.prevSlide' ),
-            nextArrow: $( '.nextSlide' ),
-        });
-
-
-        sliderWrap.width( cardItemWidth * cardItem.length );
-
-        closeButton.on( 'click', function() {
-            $( this ).closest( '.card-item' ).remove();
-            sliderWrap.width( cardItemWidth * ( $( '.card-item').length - 1));
-            comparison.prototype.heightSetter();
-        });
-
-        $carousel.on( 'swipe', function(event, slick, direction){
-            if(direction == "left") {
-                var leftPos = sliderBlock.scrollLeft();
-                sliderBlock.animate({ scrollLeft: leftPos + cardItemWidth }, 800 );
-            } else {
-                var leftPos = sliderBlock.scrollLeft();
-                sliderBlock.animate({ scrollLeft: leftPos - cardItemWidth }, 800 );
-            }
-        });
-
-        nextButton.on( 'click', function() {
-            var leftPos = sliderBlock.scrollLeft();
-            sliderBlock.animate({ scrollLeft: leftPos + cardItemWidth }, 800 );
-        });
-
-        prevButton.on( 'click', function() {
-            var leftPos = sliderBlock.scrollLeft();
-            sliderBlock.animate({ scrollLeft: leftPos - cardItemWidth }, 800 );
-        });
-    }
+    // comparison.prototype.comparisonCarousel = function() {
+    //     var $carousel = $( '.carousel-property' ),
+    //         nextButton = $( '.nextSlide' ),
+    //         prevButton = $( '.prevSlide' ),
+    //         sliderWrap = $( '.comparison-wrap'),
+    //         sliderBlock = $( '.comparison-block'),
+    //         cardItem = $( '.card-item'),
+    //         cardItemWidth = 220,
+    //         slickwrap = $( '.slick-track'),
+    //         closeButton = $( '.times' );
+    //
+    //     $carousel.slick({
+    //         slidesToShow: 1,
+    //         slidesToScroll: 1,
+    //         infinite: false,
+    //         dots: false,
+    //         accessibility: false,
+    //         swipe: true,
+    //         prevArrow: $( '.prevSlide' ),
+    //         nextArrow: $( '.nextSlide' ),
+    //     });
+    //
+    //
+    //     sliderWrap.width( cardItemWidth * cardItem.length );
+    //
+    //     closeButton.on( 'click', function() {
+    //         $( this ).closest( '.card-item' ).remove();
+    //         sliderWrap.width( cardItemWidth * ( $( '.card-item').length - 1));
+    //         comparison.prototype.heightSetter();
+    //     });
+    //
+    //     $carousel.on( 'swipe', function(event, slick, direction){
+    //         if(direction == "left") {
+    //             var leftPos = sliderBlock.scrollLeft();
+    //             sliderBlock.animate({ scrollLeft: leftPos + cardItemWidth }, 800 );
+    //         } else {
+    //             var leftPos = sliderBlock.scrollLeft();
+    //             sliderBlock.animate({ scrollLeft: leftPos - cardItemWidth }, 800 );
+    //         }
+    //     });
+    //
+    //     nextButton.on( 'click', function() {
+    //         var leftPos = sliderBlock.scrollLeft();
+    //         sliderBlock.animate({ scrollLeft: leftPos + cardItemWidth }, 800 );
+    //     });
+    //
+    //     prevButton.on( 'click', function() {
+    //         var leftPos = sliderBlock.scrollLeft();
+    //         sliderBlock.animate({ scrollLeft: leftPos - cardItemWidth }, 800 );
+    //     });
+    // }
 
     comparison.prototype.run = function() {
-        this.comparisonCarousel();
+        // this.comparisonCarousel();
         this.heightSetter();
     };
 
