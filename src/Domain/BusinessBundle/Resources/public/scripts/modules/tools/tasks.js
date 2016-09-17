f4920b53244773b6405edaca2cca04e2f91d38b4
Aleksandr Sakov <BELHARD\sakovas@dev-php7>
2016-09-17 15:34:02 +0300

    fix(IPGN-826): disable empty space at reject reason

--- src/Domain/BusinessBundle/Resources/public/scripts/modules/tools/tasks.js
+++ src/Domain/BusinessBundle/Resources/public/scripts/modules/tools/tasks.js
@@ -8,4 +8,13 @@ $( document ).ready(function() {
             $( '#rejectReasonModal textarea' ).attr( 'required', 'required' );
         }
     } );
+
+    $( '#task-form' ).on( 'submit', function( event ) {
+        if ( $( '#rejectReasonModal textarea').prop('required') && $( '#rejectReasonModal textarea' ).val().trim() == '' ) {
+            $( '#rejectReasonModal textarea').closest( '.form-group' ).addClass( 'has-error' );
+            $( '#rejectReasonModal textarea').closest( '.form-group').append( '<div class="help-block">Field shoud not be empty.</div>' );
+
+            return false;
+        }
+    });
 });
