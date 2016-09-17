8ed06586fcc45f643447a290378fc596221487c4
Aleksandr Sakov <BELHARD\sakovas@dev-php7>
2016-09-17 15:42:43 +0300

    fix(IPGN-826): correct validation message

--- src/Domain/BusinessBundle/Resources/public/scripts/modules/tools/tasks.js
+++ src/Domain/BusinessBundle/Resources/public/scripts/modules/tools/tasks.js
@@ -12,7 +12,7 @@ $( document ).ready(function() {
     $( '#task-form' ).on( 'submit', function( event ) {
         if ( $( '#rejectReasonModal textarea').prop('required') && $( '#rejectReasonModal textarea' ).val().trim() == '' ) {
             $( '#rejectReasonModal textarea').closest( '.form-group' ).addClass( 'has-error' );
-            $( '#rejectReasonModal textarea').closest( '.form-group').append( '<div class="help-block">Field shoud not be empty.</div>' );
+            $( '#rejectReasonModal textarea').closest( '.form-group').append( '<div class="help-block">Field should not be empty.</div>' );
 
             return false;
         }
