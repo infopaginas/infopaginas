e78905256e57c508a9c01e5ca386bd14581a8c1e
Gruk, Eugen <evgeniy.gruk@oxagile.com>
2016-12-19 15:52:36 +0300

    INFT-278 [User Profile] preview for logo and background images is fixed. Code style is fixed

--- src/Domain/BusinessBundle/Resources/public/scripts/modules/tools/images.js
+++ src/Domain/BusinessBundle/Resources/public/scripts/modules/tools/images.js
@@ -283,13 +283,10 @@ define(['jquery', 'bootstrap', 'tools/spin', 'tools/select'], function( $, boots
         var self = this;
 
         $(document).on( 'change', this.html.imageTypeSelectClassname, function() {
-            console.debug($( this ));
             if ( typeof logoTypeConstant !== undefined && $( this ).val() == logoTypeConstant ) {
                 var triggeredSelect = this;
 
                 $.each( $( self.html.imageTypeSelectClassname ), function() {
-                    console.debug( $( this ).val() );
-                    console.debug($( triggeredSelect ).closest( '.image-row' ).children('.hidden-media').val());
                     if ( $( this ).val() == logoTypeConstant &&
                         $( triggeredSelect ).closest( '.image-row' ).children('.hidden-media').val() != $( this ).closest( '.image-row' ).children('.hidden-media').val()
                     ) {
