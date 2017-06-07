0a017c2ba8a9c34354a9e2eb7c176d677d8ba537
skoptsovea <evgeniy.skoptsov@oxagile.com>
2017-06-07 16:15:28 +0300

    INFT-1302

--- src/Oxa/Sonata/MediaBundle/Entity/Media.php
+++ src/Oxa/Sonata/MediaBundle/Entity/Media.php
@@ -32,6 +32,11 @@ class Media extends BaseMedia implements OxaMediaInterface, DefaultEntityInterfa
     const UPLOADS_DIR_NAME = 'uploads';
 
     /**
+     * Max image size in bytes
+     */
+    const MEDIA_MAX_SIZE = 10000000;
+
+    /**
      * @var int
      *
      * @ORM\Column(name="id", type="integer")
