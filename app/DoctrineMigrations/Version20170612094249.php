80aaf6d56f32d4a3d312967b8cd222f07b8532b8
Evgeniy Skoptsov <evgeniy.skoptsov@oxagile.com>
2017-06-14 18:08:51 +0300

    Update Version20170612094249.php

--- app/DoctrineMigrations/Version20170612094249.php
+++ app/DoctrineMigrations/Version20170612094249.php
@@ -35,7 +35,7 @@ class Version20170612094249 extends AbstractMigration implements ContainerAwareI
      */
     public function up(Schema $schema)
     {
-        $this->addSql("DROP TABLE IF EXSIST subscription_report_subscription");
+        $this->addSql("DROP TABLE IF EXISTS subscription_report_subscription");
         $this->downgradeSuperVmSubscriptions();
         $this->addConfigValues();
 
