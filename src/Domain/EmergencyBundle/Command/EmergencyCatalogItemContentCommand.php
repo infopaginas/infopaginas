61c677b5ce3342b4023f9184b658b8c6d5964160
skoptsovea <evgeniy.skoptsov@oxagile.com>
2017-09-28 10:55:19 +0300

    INFT-2113 bug fix

--- src/Domain/EmergencyBundle/Command/EmergencyCatalogItemContentCommand.php
+++ src/Domain/EmergencyBundle/Command/EmergencyCatalogItemContentCommand.php
@@ -7,7 +7,8 @@ use Domain\EmergencyBundle\Entity\EmergencyArea;
 use Domain\EmergencyBundle\Entity\EmergencyBusiness;
 use Domain\EmergencyBundle\Entity\EmergencyCatalogItem;
 use Domain\EmergencyBundle\Entity\EmergencyCategory;
-use Domain\SearchBundle\Model\Manager\SearchManager;
+use Oxa\ConfigBundle\Model\ConfigInterface;
+use Oxa\ConfigBundle\Service\Config;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -19,8 +20,8 @@ class EmergencyCatalogItemContentCommand extends ContainerAwareCommand
     /* @var EntityManager $em */
     protected $em;
 
-    /* @var SearchManager $searchManager */
-    protected $searchManager;
+    /* @var Config $config */
+    protected $config;
 
     protected function configure()
     {
@@ -37,60 +38,62 @@ class EmergencyCatalogItemContentCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->em = $this->getContainer()->get('doctrine.orm.entity_manager');
-        $this->searchManager = $this->getContainer()->get('domain_search.manager.search');
+        $this->config = $this->getContainer()->get('oxa_config');
 
         $this->updateCatalogItem();
     }
 
     protected function updateCatalogItem()
     {
-        $catalogAreas = $this->em->getRepository(EmergencyArea::class)->getAllAreasIterator();
+        if ($this->getEmergencyFeatureEnabled()) {
+            $catalogAreas = $this->em->getRepository(EmergencyArea::class)->getAllAreasIterator();
 
-        foreach ($catalogAreas as $areaRow) {
-            /* @var $catalogArea EmergencyArea */
-            $catalogArea = current($areaRow);
+            foreach ($catalogAreas as $areaRow) {
+                /* @var $catalogArea EmergencyArea */
+                $catalogArea = current($areaRow);
 
-            $categories = $this->em->getRepository(EmergencyCategory::class)->getAllCategoriesIterator();
+                $categories = $this->em->getRepository(EmergencyCategory::class)->getAllCategoriesIterator();
 
-            $countAreaContent = 0;
+                $countAreaContent = 0;
 
-            foreach ($categories as $categoryRow) {
-                /* @var $category EmergencyCategory */
-                $category = current($categoryRow);
+                foreach ($categories as $categoryRow) {
+                    /* @var $category EmergencyCategory */
+                    $category = current($categoryRow);
+
+                    $catalogItem = $this->em->getRepository(EmergencyCatalogItem::class)->findOneBy(
+                        [
+                            'area'     => $catalogArea->getId(),
+                            'category' => $category->getId(),
+                        ]
+                    );
+
+                    if (!$catalogItem) {
+                        $catalogItem = $this->createCatalogItem($catalogArea, $category);
+                    }
+
+                    $countCategoryContent = $this->getCountCatalogItemContent($catalogArea, $category);
+
+                    $catalogItem->setHasContent((bool)$countCategoryContent);
+
+                    $countAreaContent += $countCategoryContent;
+                }
 
                 $catalogItem = $this->em->getRepository(EmergencyCatalogItem::class)->findOneBy(
                     [
                         'area'     => $catalogArea->getId(),
-                        'category' => $category->getId(),
+                        'category' => null,
                     ]
                 );
 
                 if (!$catalogItem) {
-                    $catalogItem = $this->createCatalogItem($catalogArea, $category);
+                    $catalogItem = $this->createCatalogItem($catalogArea);
                 }
 
-                $countCategoryContent = $this->getCountCatalogItemContent($catalogArea, $category);
+                $catalogItem->setHasContent((bool)$countAreaContent);
 
-                $catalogItem->setHasContent((bool)$countCategoryContent);
-
-                $countAreaContent += $countCategoryContent;
+                $this->em->flush();
+                $this->em->clear();
             }
-
-            $catalogItem = $this->em->getRepository(EmergencyCatalogItem::class)->findOneBy(
-                [
-                    'area'     => $catalogArea->getId(),
-                    'category' => null,
-                ]
-            );
-
-            if (!$catalogItem) {
-                $catalogItem = $this->createCatalogItem($catalogArea);
-            }
-
-            $catalogItem->setHasContent((bool)$countAreaContent);
-
-            $this->em->flush();
-            $this->em->clear();
         }
     }
 
@@ -121,5 +124,13 @@ class EmergencyCatalogItemContentCommand extends ContainerAwareCommand
         $this->em->persist($catalogItem);
 
         return $catalogItem;
+    }
+
+    /**
+     * @return bool
+     */
+    protected function getEmergencyFeatureEnabled()
+    {
+        return (bool)$this->config->getSetting(ConfigInterface::EMERGENCY_SITUATION_ON)->getValue();
     }
 }
