67713e76d7a3426c7272edd4e63cb172356fe21a
Alekseyuk Sergey <sergey.alekseyuk@oxagile.com>
2018-07-05 12:21:59 +0300

    INFT-2761 [IP][Export]: When the user performs export of report in .pdf the file's extension is .file

--- src/Domain/ReportBundle/Service/Export/BusinessChartReportPdfExporter.php
+++ src/Domain/ReportBundle/Service/Export/BusinessChartReportPdfExporter.php
@@ -25,7 +25,7 @@ class BusinessChartReportPdfExporter extends PdfExporterModel
             $businessName = SlugUtil::convertSlug($params['businessProfile']->getName());
         }
 
-        $filename = $businessName . '_' . date(self::DATE_EXPORT_FORMAT);
+        $filename = $businessName . '_' . date(self::DATE_EXPORT_FORMAT) . '.pdf';
 
         $html = $this->templateEngine->render(
             'DomainReportBundle:PDF:charts-template.html.twig',
