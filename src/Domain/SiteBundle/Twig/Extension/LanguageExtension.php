b4dbf14bc8afe85f370dc2b5e2d62ffb15bd0011
tivunovdv <dmitriy.tivunov@oxagile.com>
2019-05-30 11:12:22 +0000

    INFT-3071 - Language twig extension added

---
+++ src/Domain/SiteBundle/Twig/Extension/LanguageExtension.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Domain\SiteBundle\Twig\Extension;
+
+
+class LanguageExtension extends \Twig_Extension
+{
+    private $defaultLanguage;
+
+    public function setDefaultLanguage(string $defaultLanguage)
+    {
+        $this->defaultLanguage = $defaultLanguage;
+    }
+
+    public function getFunctions()
+    {
+        return [
+            'get_current_language' => new \Twig_Function_Method(
+                $this,
+                'getCurrentLanguage',
+                ['needs_environment' => true]
+            ),
+        ];
+    }
+
+    public function getCurrentLanguage(\Twig_Environment $environment)
+    {
+        if (isset($environment->getGlobals()['languages'])) {
+            foreach ($environment->getGlobals()['languages'] as $key => $language) {
+                if (isset($language['active']) && $language['active']) {
+                    return $key;
+                }
+            }
+        }
+
+        return $this->defaultLanguage;
+    }
+
+    public function getName() : string
+    {
+        return 'language_extension';
+    }
+}
