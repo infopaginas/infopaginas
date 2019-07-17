baf3d3daac772f6d583834d4243267bccf4ad2b2
tivunovdv <dmitriy.tivunov@oxagile.com>
2019-07-17 10:44:44 +0000

    INFT-3134 - code style fixed

--- src/Domain/BusinessBundle/Manager/HomepageCarouselManager.php
+++ src/Domain/BusinessBundle/Manager/HomepageCarouselManager.php
@@ -2,19 +2,13 @@
 
 namespace Domain\BusinessBundle\Manager;
 
-use Domain\BusinessBundle\Entity\HomepageCarousel;
 use Oxa\ManagerArchitectureBundle\Model\Manager\Manager;
 
 class HomepageCarouselManager extends Manager
 {
     public function getCarouselBusinessesSortedByPosition()
     {
-        $carouselBusinesses = $this->em->getRepository(HomepageCarousel::class)->findBy(
-            [],
-            ['position' => 'ASC']
-        );
-
-        return $carouselBusinesses;
+        return $this->getRepository()->findBy([], ['position' => 'ASC']);
     }
 
     public function isShowCarousel($carouselBusinesses)
