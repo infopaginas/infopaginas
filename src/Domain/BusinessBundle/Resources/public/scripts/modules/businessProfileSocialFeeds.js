3103e4c75a1c58c7ff2c57a7ecb6fc50ae5b3a5d
Evgeniy Skoptsov <evgeniy.skopzov@oxagile.com>
2019-08-14 20:40:42 +0300

    INFT-3154 - change styles

--- src/Domain/BusinessBundle/Resources/public/scripts/modules/businessProfileSocialFeeds.js
+++ src/Domain/BusinessBundle/Resources/public/scripts/modules/businessProfileSocialFeeds.js
@@ -15,11 +15,12 @@ define( [ 'jquery' ], function ( $ ) {
         this.run();
     };
 
-    socialFeeds.prototype.insertInstagramPosts = function ( url ) {
-        if ( url ) {
+    socialFeeds.prototype.insertInstagramPosts = function ( urlSting ) {
+        if ( urlSting ) {
             var that = this;
-
-            $.get( url + '?__a=1' )
+            var url = new URL(urlSting);
+            url.searchParams.append('__a', '1');
+            $.get( url )
                 .done( function ( data ) {
                         $.each( data.graphql.user.edge_owner_to_timeline_media.edges, function ( index, value ) {
                             $.get( that.html.instagramEmbedPostEndpoint + value.node.shortcode + that.html.instagramEmbedPostParameters )
@@ -34,7 +35,8 @@ define( [ 'jquery' ], function ( $ ) {
 
     socialFeeds.prototype.setFBWidth = function () {
         if ( typeof FB !== 'undefined' ) {
-            $( '.fb-page' ).attr( 'data-width', $( '.social-feeds-column' ).width() );
+            $( '.fb-page' ).attr( 'data-width', $( '.social-feeds-column' ).width() )
+                .removeClass( 'fb_iframe_widget fb_iframe_widget_fluid' );
             FB.XFBML.parse();
         }
     };
