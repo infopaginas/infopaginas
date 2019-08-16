63b95198056668947a8202f6662119ac377c46b3
Evgeniy Skoptsov <evgeniy.skopzov@oxagile.com>
2019-08-16 14:14:48 +0300

    INFT-3154 - add lazy loading for social networks feeds

--- src/Domain/BusinessBundle/Resources/public/scripts/modules/businessProfileSocialFeeds.js
+++ src/Domain/BusinessBundle/Resources/public/scripts/modules/businessProfileSocialFeeds.js
@@ -7,7 +7,12 @@ define( [ 'jquery' ], function ( $ ) {
             instagramPostsWrapperClass: '.posts-wrapper',
 
             instagramEmbedPostEndpoint: 'https://api.instagram.com/oembed/?url=http://instagr.am/p/',
-            instagramEmbedPostParameters: '&omitscript=true'
+            instagramEmbedPostParameters: '&omitscript=true',
+            FBPostsWrapperClass: 'fb-page',
+            FBPagePlaceholder: '#fb-page-placeholder',
+            lazyloadElementClass: 'lazy',
+            twitterPlaceholder: '#twitter-timeline-placeholder',
+            twitterTimelineClass: 'twitter-timeline',
         };
 
         this.instagramURL = $( this.html.instagramFeedClass ).data( 'instagram-url' );
@@ -35,16 +40,63 @@ define( [ 'jquery' ], function ( $ ) {
 
     socialFeeds.prototype.setFBWidth = function () {
         if ( typeof FB !== 'undefined' ) {
-            $( '.fb-page' ).attr( 'data-width', $( '.social-feeds-column' ).width() )
+            $( '.' + this.html.FBPostsWrapperClass ).attr( 'data-width', $( '.social-feeds-column' ).width() )
                 .removeClass( 'fb_iframe_widget fb_iframe_widget_fluid' );
             FB.XFBML.parse();
         }
     };
 
-    socialFeeds.prototype.run = function () {
-        this.insertInstagramPosts( this.instagramURL );
+    socialFeeds.prototype.initFBPlugin = function () {
+        $( this.html.FBPagePlaceholder ).addClass( this.html.FBPostsWrapperClass );
         this.setFBWidth();
-        window.addEventListener('resize', this.setFBWidth);
+        window.addEventListener( 'resize', $.proxy( this.setFBWidth, this ) );
+    };
+
+    socialFeeds.prototype.showFeedsIfVisible = function () {
+        var instagramWrapper = $( this.html.instagramFeedClass + ' ' + this.html.instagramPostsWrapperClass );
+        var FBWrapper = $( this.html.FBPagePlaceholder );
+        var twitterPlaceholder = $( this.html.twitterPlaceholder );
+
+        if ( instagramWrapper.hasClass( this.html.lazyloadElementClass ) && this.isScrolledIntoView( instagramWrapper ) ) {
+            this.insertInstagramPosts( this.instagramURL );
+            instagramWrapper.removeClass( this.html.lazyloadElementClass );
+        }
+
+        if ( FBWrapper.hasClass( this.html.lazyloadElementClass ) && this.isScrolledIntoView( FBWrapper ) ) {
+            this.initFBPlugin();
+            FBWrapper.removeClass( this.html.lazyloadElementClass );
+        }
+
+        if ( twitterPlaceholder.hasClass( this.html.lazyloadElementClass ) && this.isScrolledIntoView( twitterPlaceholder ) ) {
+            twitterPlaceholder.removeClass( this.html.lazyloadElementClass ).addClass( this.html.twitterTimelineClass );
+            twttr.widgets.load();
+        }
+
+        if ( $( '.' + this.html.lazyloadElementClass ).length === 0 ) {
+            window.removeEventListener( 'scroll', this.showFeedsIfVisibleProxy );
+            window.removeEventListener( 'resize', this.showFeedsIfVisibleProxy );
+            window.removeEventListener( 'orientationChange', this.showFeedsIfVisibleProxy );
+        }
+    };
+
+    socialFeeds.prototype.isScrolledIntoView = function ( element, fullyInView ) {
+        var pageTop = $( window ).scrollTop();
+        var pageBottom = pageTop + $( window ).height();
+        var elementTop = $( element ).offset().top;
+        var elementBottom = elementTop + $( element ).height();
+
+        if ( fullyInView === true ) {
+            return ((pageTop < elementTop) && (pageBottom > elementBottom));
+        } else {
+            return ((elementTop <= pageBottom) && (elementBottom >= pageTop));
+        }
+    };
+
+    socialFeeds.prototype.run = function () {
+        this.showFeedsIfVisibleProxy = $.proxy( this.showFeedsIfVisible, this );
+        window.addEventListener( 'scroll', this.showFeedsIfVisibleProxy );
+        window.addEventListener( 'resize', this.showFeedsIfVisibleProxy );
+        window.addEventListener( 'orientationChange', this.showFeedsIfVisibleProxy );
     };
 
     return socialFeeds;
