b504d51c264bd8e4aeff88c5cde0c2f46624aba1
cgomez9 <cgomez@infopaginas.com>
2019-08-28 09:01:34 -0400

    INFT-3190 - Suggested changes

--- src/Domain/ReportBundle/Manager/SocialNetworksReportManager.php
+++ src/Domain/ReportBundle/Manager/SocialNetworksReportManager.php
@@ -17,16 +17,7 @@ class SocialNetworksReportManager
     /** @var MongoDbManager $mongoDbManager */
     protected $mongoDbManager;
 
-    const MONGO_DB_COLLECTION_NAME_RAW       = 'overview_raw';
     const MONGO_DB_COLLECTION_NAME_AGGREGATE = 'overview_aggregate';
-
-    const MONGO_DB_COLLECTION_NAME_ARCHIVE_RAW       = 'overview_archive_raw';
-    const MONGO_DB_COLLECTION_NAME_ARCHIVE_AGGREGATE = 'overview_archive_aggregate';
-
-    const MONGO_DB_FIELD_ACTION    = 'action';
-    const MONGO_DB_FIELD_BUSINESS_ID = 'business_id';
-    const MONGO_DB_FIELD_COUNT       = 'count';
-    const MONGO_DB_FIELD_DATE_TIME   = 'datetime';
 
     /**
      * @param MongoDbManager $mongoDbManager
@@ -52,33 +43,6 @@ class SocialNetworksReportManager
         ];
 
         return $keywordsData;
-    }
-
-    /**
-     * @param array $data
-     *
-     * @return bool
-     */
-    public function registerBusinessKeywordEvent($data)
-    {
-        foreach ($data as $search => $businessIds) {
-            $this->saveProfilesDataSuggestedBySearchQuery($search, $businessIds);
-        }
-
-        return true;
-    }
-
-    /**
-     * @param string $search
-     * @param array  $businessIds
-     */
-    public function saveProfilesDataSuggestedBySearchQuery($search, $businessIds)
-    {
-        $keywords = mb_strtolower($search);
-
-        $data = $this->buildBusinessSocialNetworks($keywords, $businessIds);
-
-        $this->insertBusinessKeywords($data);
     }
 
     /**
@@ -118,11 +82,11 @@ class SocialNetworksReportManager
             [
                 [
                     '$match' => [
-                        self::MONGO_DB_FIELD_BUSINESS_ID => (int)$params['businessProfileId'],
-                        self::MONGO_DB_FIELD_ACTION => [
+                        BusinessOverviewReportManager::MONGO_DB_FIELD_BUSINESS_ID => (int)$params['businessProfileId'],
+                        BusinessOverviewReportManager::MONGO_DB_FIELD_ACTION => [
                             '$in' => $socialNetworksActions
                         ],
-                        self::MONGO_DB_FIELD_DATE_TIME => [
+                        BusinessOverviewReportManager::MONGO_DB_FIELD_DATE_TIME => [
                             '$gte' => $this->mongoDbManager->typeUTCDateTime($params['dateObject']->getStartDate()),
                             '$lte' => $this->mongoDbManager->typeUTCDateTime($params['dateObject']->getEndDate()),
                         ],
@@ -130,15 +94,15 @@ class SocialNetworksReportManager
                 ],
                 [
                     '$group' => [
-                        '_id' => '$' . self::MONGO_DB_FIELD_ACTION,
-                        self::MONGO_DB_FIELD_COUNT => [
-                            '$sum' => '$' . self::MONGO_DB_FIELD_COUNT,
+                        '_id' => '$' . BusinessOverviewReportManager::MONGO_DB_FIELD_ACTION,
+                        BusinessOverviewReportManager::MONGO_DB_FIELD_COUNT => [
+                            '$sum' => '$' . BusinessOverviewReportManager::MONGO_DB_FIELD_COUNT,
                         ],
                     ],
                 ],
                 [
                     '$sort'  => [
-                        self::MONGO_DB_FIELD_COUNT => -1,
+                        BusinessOverviewReportManager::MONGO_DB_FIELD_COUNT => -1,
                     ],
                 ],
                 [
@@ -149,164 +113,9 @@ class SocialNetworksReportManager
 
         $result = [];
         foreach ($cursor as $document) {
-            $result[$document['_id']] = $document[self::MONGO_DB_FIELD_COUNT];
+            $result[$document['_id']] = $document[BusinessOverviewReportManager::MONGO_DB_FIELD_COUNT];
         }
 
         return $result;
-    }
-
-    /**
-     * @param int       $businessId
-     * @param string    $socialNetwork
-     * @param MongoDB\BSON\UTCDateTime $date
-     *
-     * @return array
-     */
-    protected function buildSingleBusinessSocialNetwork($businessId, $socialNetwork, $date)
-    {
-        $data = [
-            self::MONGO_DB_FIELD_BUSINESS_ID => (int)$businessId,
-            self::MONGO_DB_FIELD_ACTION     => $socialNetwork,
-            self::MONGO_DB_FIELD_DATE_TIME   => $date,
-        ];
-
-        return $data;
-    }
-
-    /**
-     * @param string $keywords
-     * @param array  $businessIds
-     *
-     * @return array
-     */
-    protected function buildBusinessSocialNetworks($socialNetwork, $businessIds)
-    {
-        $data = [];
-        $date = $this->mongoDbManager->typeUTCDateTime(new \DateTime());
-
-        foreach ($businessIds as $businessId) {
-            $data[] = $this->buildSingleBusinessSocialNetwork($businessId, $socialNetwork, $date);
-        }
-
-        $data[] = $this->buildSingleBusinessSocialNetwork(0, $socialNetwork, $date);
-
-        return $data;
-    }
-
-    /**
-     * @param array $data
-     */
-    protected function insertBusinessKeywords($data)
-    {
-        $this->mongoDbManager->insertMany(
-            self::MONGO_DB_COLLECTION_NAME_RAW,
-            $data
-        );
-    }
-
-    /**
-     * @param ReportDatesRangeVO $period
-     */
-    public function aggregateBusinessSocialNetworks($period)
-    {
-        $this->mongoDbManager->createIndex(self::MONGO_DB_COLLECTION_NAME_AGGREGATE, [
-            self::MONGO_DB_FIELD_BUSINESS_ID => MongoDbManager::INDEX_TYPE_ASC,
-            self::MONGO_DB_FIELD_DATE_TIME   => MongoDbManager::INDEX_TYPE_DESC,
-        ]);
-
-        $aggregateStartDate = $this->mongoDbManager->typeUTCDateTime($period->getStartDate());
-        $aggregateEndDate   = $this->mongoDbManager->typeUTCDateTime($period->getEndDate());
-
-        $cursor = $this->mongoDbManager->aggregateData(
-            self::MONGO_DB_COLLECTION_NAME_RAW,
-            [
-                [
-                    '$match' => [
-                        self::MONGO_DB_FIELD_DATE_TIME => [
-                            '$gte' => $aggregateStartDate,
-                            '$lte' => $aggregateEndDate,
-                        ],
-                    ],
-                ],
-                [
-                    '$project' => [
-                        'query' => [
-                            'action' => '$' . self::MONGO_DB_FIELD_ACTION,
-                            'bid'     => '$' . self::MONGO_DB_FIELD_BUSINESS_ID,
-                        ],
-                    ]
-                ],
-                [
-                    '$group' => [
-                        '_id' => '$query',
-                        self::MONGO_DB_FIELD_COUNT => [
-                            '$sum' => 1,
-                        ],
-                    ],
-                ]
-            ]
-        );
-
-        $i = 0;
-        $insert = [];
-
-        foreach ($cursor as $document) {
-            $document[self::MONGO_DB_FIELD_ACTION]     = $document['_id']['action'];
-            $document[self::MONGO_DB_FIELD_BUSINESS_ID] = $document['_id']['bid'];
-            $document[self::MONGO_DB_FIELD_DATE_TIME]   = $aggregateStartDate;
-
-            $document['_id'] = $this->mongoDbManager->generateId();
-
-            $insert[] = $document;
-
-            if (($i % MongoDbManager::DEFAULT_BATCH_SIZE) === 0) {
-                $this->mongoDbManager->insertMany(self::MONGO_DB_COLLECTION_NAME_AGGREGATE, $insert);
-                $insert = [];
-            }
-
-            $i++;
-        }
-
-        if ($insert) {
-            $this->mongoDbManager->insertMany(self::MONGO_DB_COLLECTION_NAME_AGGREGATE, $insert);
-        }
-    }
-
-    /**
-     * @param \Datetime $date
-     */
-    public function archiveRawBusinessSocialNetworks($date)
-    {
-        $this->mongoDbManager->archiveCollection(
-            self::MONGO_DB_COLLECTION_NAME_RAW,
-            self::MONGO_DB_COLLECTION_NAME_ARCHIVE_RAW,
-            self::MONGO_DB_FIELD_DATE_TIME,
-            $date
-        );
-    }
-
-    /**
-     * @param \Datetime $date
-     */
-    public function archiveAggregatedBusinessSocialNetworks($date)
-    {
-        $this->mongoDbManager->archiveCollection(
-            self::MONGO_DB_COLLECTION_NAME_AGGREGATE,
-            self::MONGO_DB_COLLECTION_NAME_ARCHIVE_AGGREGATE,
-            self::MONGO_DB_FIELD_DATE_TIME,
-            $date
-        );
-    }
-
-    /**
-     * @param \Datetime $date
-     */
-    public function deleteArchivedRawSocialNetworksData($date)
-    {
-        $this->mongoDbManager->deleteOldData(
-            self::MONGO_DB_COLLECTION_NAME_ARCHIVE_RAW,
-            self::MONGO_DB_FIELD_DATE_TIME,
-            $date
-        );
     }
 }
--- src/Domain/ReportBundle/Model/BusinessOverviewModel.php
+++ src/Domain/ReportBundle/Model/BusinessOverviewModel.php
@@ -5,65 +5,64 @@ namespace Domain\ReportBundle\Model;
 class BusinessOverviewModel implements ReportInterface
 {
     const TYPE_CODE_IMPRESSION = 'impressions';
-    const TYPE_CODE_VIEW = 'views';
-    const TYPE_CODE_KEYWORD = 'keyword';
-    const TYPE_CODE_ADS = 'ads';
-    const TYPE_CODE_CATEGORY_BUSINESS = 'category_business';
-    const TYPE_CODE_CATEGORY_CATALOG = 'category_catalog';
-    const TYPE_CODE_SOCIAL_NETWORKS = 'social_networks';
+    const TYPE_CODE_VIEW       = 'views';
+    const TYPE_CODE_KEYWORD    = 'keyword';
+    const TYPE_CODE_ADS        = 'ads';
+    const TYPE_CODE_CATEGORY_BUSINESS   = 'category_business';
+    const TYPE_CODE_CATEGORY_CATALOG    = 'category_catalog';
 
-    const TYPE_CODE_DIRECTION_BUTTON = 'directionButton';
-    const TYPE_CODE_MAP_SHOW_BUTTON = 'mapShowButton';
-    const TYPE_CODE_MAP_MARKER_BUTTON = 'mapMarkerButton';
-    const TYPE_CODE_WEB_BUTTON = 'webButton';
-    const TYPE_CODE_WEB_ACTION_BUTTON = 'webActionButton';
-    const TYPE_CODE_CALL_MOB_BUTTON = 'callMobButton';
-    const TYPE_CODE_CALL_DESK_BUTTON = 'callDeskButton';
-    const TYPE_CODE_ADD_COMPARE_BUTTON = 'addCompareButton';
-    const TYPE_CODE_REMOVE_COMPARE_BUTTON = 'removeCompareButton';
+    const TYPE_CODE_DIRECTION_BUTTON       = 'directionButton';
+    const TYPE_CODE_MAP_SHOW_BUTTON        = 'mapShowButton';
+    const TYPE_CODE_MAP_MARKER_BUTTON      = 'mapMarkerButton';
+    const TYPE_CODE_WEB_BUTTON             = 'webButton';
+    const TYPE_CODE_WEB_ACTION_BUTTON      = 'webActionButton';
+    const TYPE_CODE_CALL_MOB_BUTTON        = 'callMobButton';
+    const TYPE_CODE_CALL_DESK_BUTTON       = 'callDeskButton';
+    const TYPE_CODE_ADD_COMPARE_BUTTON     = 'addCompareButton';
+    const TYPE_CODE_REMOVE_COMPARE_BUTTON  = 'removeCompareButton';
 
-    const TYPE_CODE_FACEBOOK_SHARE = 'facebookShare';
-    const TYPE_CODE_TWITTER_SHARE = 'twitterShare';
+    const TYPE_CODE_FACEBOOK_SHARE  = 'facebookShare';
+    const TYPE_CODE_TWITTER_SHARE   = 'twitterShare';
 
-    const TYPE_CODE_LINKED_IN_VISIT = 'linkedInVisit';
-    const TYPE_CODE_FACEBOOK_VISIT = 'facebookVisit';
-    const TYPE_CODE_TWITTER_VISIT = 'twitterVisit';
-    const TYPE_CODE_GOOGLE_VISIT = 'googleVisit';
-    const TYPE_CODE_YOUTUBE_VISIT = 'youtubeVisit';
-    const TYPE_CODE_INSTAGRAM_VISIT = 'instagramVisit';
+    const TYPE_CODE_LINKED_IN_VISIT    = 'linkedInVisit';
+    const TYPE_CODE_FACEBOOK_VISIT     = 'facebookVisit';
+    const TYPE_CODE_TWITTER_VISIT      = 'twitterVisit';
+    const TYPE_CODE_GOOGLE_VISIT       = 'googleVisit';
+    const TYPE_CODE_YOUTUBE_VISIT      = 'youtubeVisit';
+    const TYPE_CODE_INSTAGRAM_VISIT    = 'instagramVisit';
     const TYPE_CODE_TRIP_ADVISOR_VISIT = 'tripAdvisorVisit';
 
-    const TYPE_CODE_VIDEO_WATCHED = 'videoWatched';
-    const TYPE_CODE_REVIEW_CLICK = 'reviewClick';
-    const TYPE_CODE_EMAIL_CLICK = 'emailClick';
+    const TYPE_CODE_VIDEO_WATCHED  = 'videoWatched';
+    const TYPE_CODE_REVIEW_CLICK   = 'reviewClick';
+    const TYPE_CODE_EMAIL_CLICK    = 'emailClick';
 
     const EVENT_TYPES = [
-        self::TYPE_CODE_VIEW => 'interaction_report.event.view',
-        self::TYPE_CODE_IMPRESSION => 'interaction_report.event.impression',
-        self::TYPE_CODE_DIRECTION_BUTTON => 'interaction_report.button.direction',
-        self::TYPE_CODE_MAP_SHOW_BUTTON => 'interaction_report.button.show_map',
-        self::TYPE_CODE_MAP_MARKER_BUTTON => 'interaction_report.button.marker_map',
-        self::TYPE_CODE_WEB_BUTTON => 'interaction_report.button.web',
-        self::TYPE_CODE_WEB_ACTION_BUTTON => 'interaction_report.button.web_action',
-        self::TYPE_CODE_CALL_MOB_BUTTON => 'interaction_report.button.call_mob',
-        self::TYPE_CODE_CALL_DESK_BUTTON => 'interaction_report.button.call_desk',
-        self::TYPE_CODE_ADD_COMPARE_BUTTON => 'interaction_report.button.add_compare',
+        self::TYPE_CODE_VIEW                  => 'interaction_report.event.view',
+        self::TYPE_CODE_IMPRESSION            => 'interaction_report.event.impression',
+        self::TYPE_CODE_DIRECTION_BUTTON      => 'interaction_report.button.direction',
+        self::TYPE_CODE_MAP_SHOW_BUTTON       => 'interaction_report.button.show_map',
+        self::TYPE_CODE_MAP_MARKER_BUTTON     => 'interaction_report.button.marker_map',
+        self::TYPE_CODE_WEB_BUTTON            => 'interaction_report.button.web',
+        self::TYPE_CODE_WEB_ACTION_BUTTON     => 'interaction_report.button.web_action',
+        self::TYPE_CODE_CALL_MOB_BUTTON       => 'interaction_report.button.call_mob',
+        self::TYPE_CODE_CALL_DESK_BUTTON      => 'interaction_report.button.call_desk',
+        self::TYPE_CODE_ADD_COMPARE_BUTTON    => 'interaction_report.button.add_compare',
         self::TYPE_CODE_REMOVE_COMPARE_BUTTON => 'interaction_report.button.remove_compare',
-        self::TYPE_CODE_FACEBOOK_SHARE => 'interaction_report.share.facebook',
-        self::TYPE_CODE_TWITTER_SHARE => 'interaction_report.share.twitter',
-        self::TYPE_CODE_LINKED_IN_VISIT => 'interaction_report.visit.linked_in',
-        self::TYPE_CODE_FACEBOOK_VISIT => 'interaction_report.visit.facebook',
-        self::TYPE_CODE_TWITTER_VISIT => 'interaction_report.visit.twitter',
-        self::TYPE_CODE_GOOGLE_VISIT => 'interaction_report.visit.google',
-        self::TYPE_CODE_YOUTUBE_VISIT => 'interaction_report.visit.youtube',
-        self::TYPE_CODE_INSTAGRAM_VISIT => 'interaction_report.visit.instagram',
-        self::TYPE_CODE_TRIP_ADVISOR_VISIT => 'interaction_report.visit.tripadvisor',
-        self::TYPE_CODE_VIDEO_WATCHED => 'interaction_report.video.watched',
-        self::TYPE_CODE_REVIEW_CLICK => 'interaction_report.review.click',
-        self::TYPE_CODE_EMAIL_CLICK => 'interaction_report.email.click',
+        self::TYPE_CODE_FACEBOOK_SHARE        => 'interaction_report.share.facebook',
+        self::TYPE_CODE_TWITTER_SHARE         => 'interaction_report.share.twitter',
+        self::TYPE_CODE_LINKED_IN_VISIT       => 'interaction_report.visit.linked_in',
+        self::TYPE_CODE_FACEBOOK_VISIT        => 'interaction_report.visit.facebook',
+        self::TYPE_CODE_TWITTER_VISIT         => 'interaction_report.visit.twitter',
+        self::TYPE_CODE_GOOGLE_VISIT          => 'interaction_report.visit.google',
+        self::TYPE_CODE_YOUTUBE_VISIT         => 'interaction_report.visit.youtube',
+        self::TYPE_CODE_INSTAGRAM_VISIT       => 'interaction_report.visit.instagram',
+        self::TYPE_CODE_TRIP_ADVISOR_VISIT    => 'interaction_report.visit.tripadvisor',
+        self::TYPE_CODE_VIDEO_WATCHED         => 'interaction_report.video.watched',
+        self::TYPE_CODE_REVIEW_CLICK          => 'interaction_report.review.click',
+        self::TYPE_CODE_EMAIL_CLICK           => 'interaction_report.email.click',
     ];
 
-    const EVENT_PRIORITY_MAIN = 'main';
+    const EVENT_PRIORITY_MAIN   = 'main';
     const EVENT_PRIORITY_COMMON = 'common';
     const EVENT_PRIORITY_HIDDEN = 'hidden';
 
@@ -75,7 +74,7 @@ class BusinessOverviewModel implements ReportInterface
     public static function getExportFormats()
     {
         return [
-            self::CODE_PDF_BUSINESS_OVERVIEW_REPORT => self::FORMAT_PDF,
+            self::CODE_PDF_BUSINESS_OVERVIEW_REPORT   => self::FORMAT_PDF,
             self::CODE_EXCEL_BUSINESS_OVERVIEW_REPORT => self::FORMAT_EXCEL,
         ];
     }
@@ -99,7 +98,6 @@ class BusinessOverviewModel implements ReportInterface
         return [
             self::TYPE_CODE_IMPRESSION,
             self::TYPE_CODE_VIEW,
-            self::TYPE_CODE_SOCIAL_NETWORKS,
             self::TYPE_CODE_DIRECTION_BUTTON,
             self::TYPE_CODE_MAP_SHOW_BUTTON,
             self::TYPE_CODE_MAP_MARKER_BUTTON,
@@ -198,7 +196,7 @@ class BusinessOverviewModel implements ReportInterface
 
         $result = array_intersect_key(self::EVENT_TYPES, array_flip($allowedEvents));
 
-        $result[self::TYPE_CODE_ADS] = 'report.type.ads';
+        $result[self::TYPE_CODE_ADS]     = 'report.type.ads';
         $result[self::TYPE_CODE_KEYWORD] = 'report.type.keywords';
 
         return $result;
@@ -210,12 +208,12 @@ class BusinessOverviewModel implements ReportInterface
     public static function getChartHints()
     {
         return [
-            self::TYPE_CODE_IMPRESSION => 'interaction_report.hint.impression',
-            self::TYPE_CODE_VIEW => 'interaction_report.hint.view',
-            self::TYPE_CODE_CALL_MOB_BUTTON => 'interaction_report.hint.call_mob',
+            self::TYPE_CODE_IMPRESSION       => 'interaction_report.hint.impression',
+            self::TYPE_CODE_VIEW             => 'interaction_report.hint.view',
+            self::TYPE_CODE_CALL_MOB_BUTTON  => 'interaction_report.hint.call_mob',
             self::TYPE_CODE_CALL_DESK_BUTTON => 'interaction_report.hint.call_desk',
             self::TYPE_CODE_DIRECTION_BUTTON => 'interaction_report.hint.direction',
-            self::TYPE_CODE_VIDEO_WATCHED => 'interaction_report.hint.video_watched',
+            self::TYPE_CODE_VIDEO_WATCHED    => 'interaction_report.hint.video_watched',
         ];
     }
 
@@ -241,8 +239,8 @@ class BusinessOverviewModel implements ReportInterface
     public static function getActionsUserData()
     {
         return [
-            self::TYPE_CODE_IMPRESSION => self::EVENT_TYPES[self::TYPE_CODE_IMPRESSION],
-            self::TYPE_CODE_CALL_MOB_BUTTON => 'Calls',
+            self::TYPE_CODE_IMPRESSION       => self::EVENT_TYPES[self::TYPE_CODE_IMPRESSION],
+            self::TYPE_CODE_CALL_MOB_BUTTON  => 'Calls',
             self::TYPE_CODE_DIRECTION_BUTTON => 'Direction',
         ];
     }
@@ -253,8 +251,8 @@ class BusinessOverviewModel implements ReportInterface
     public static function getActionTooltip()
     {
         return [
-            self::TYPE_CODE_IMPRESSION => 'user_profile.tooltip.impression',
-            self::TYPE_CODE_CALL_MOB_BUTTON => 'user_profile.tooltip.call_mob_button',
+            self::TYPE_CODE_IMPRESSION       => 'user_profile.tooltip.impression',
+            self::TYPE_CODE_CALL_MOB_BUTTON  => 'user_profile.tooltip.call_mob_button',
             self::TYPE_CODE_DIRECTION_BUTTON => 'user_profile.tooltip.direction_button',
         ];
     }
