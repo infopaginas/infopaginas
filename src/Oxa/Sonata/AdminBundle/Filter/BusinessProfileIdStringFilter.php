b5636699238acc5bbf724d8afaecc089a5d55894
Carlos <cgomez@infopaginas.com>
2020-06-15 16:41:18 -0400

    INFT-3404 - Allow "," character in CMS id search

---
+++ src/Oxa/Sonata/AdminBundle/Filter/BusinessProfileIdStringFilter.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Oxa\Sonata\AdminBundle\Filter;
+
+use Oxa\Sonata\AdminBundle\Util\Helpers\AdminHelper;
+use Sonata\AdminBundle\Datagrid\ProxyQueryInterface;
+use Sonata\AdminBundle\Form\Type\Operator\ContainsOperatorType;
+use Sonata\DoctrineORMAdminBundle\Filter\StringFilter;
+
+class BusinessProfileIdStringFilter extends StringFilter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function filter(ProxyQueryInterface $queryBuilder, $alias, $field, $data)
+    {
+        //dump($queryBuilder, $alias, $field, $data);die;
+        if (!$data || !is_array($data) || !array_key_exists('value', $data)) {
+            return;
+        }
+
+        if (strlen($data['value']) == 0) {
+            return;
+        }
+
+        // Remove comma and convert to integer
+        $data['value'] = (int)str_replace(",", "", trim($data['value']));
+
+        $parameterName = $this->getNewParameterName($queryBuilder);
+
+        $this->applyWhere(
+            $queryBuilder,
+            sprintf('%s.%s = :%s', $alias, $field, $parameterName)
+        );
+
+        $queryBuilder->setParameter($parameterName, $data['value']);
+    }
+}
