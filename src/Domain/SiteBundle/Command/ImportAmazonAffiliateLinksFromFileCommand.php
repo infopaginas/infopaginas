6a4f7596d4591273b7157a16cb7db53937fc0943
Sergei Tverdokhleb <sergey.tverdokhleb@oxagile.com>
2020-08-07 18:35:58 +0300

    INFT-3453 - Add command to import amazon affiliate links from file

--- src/Domain/SiteBundle/Command/ImportAmazonAffiliateLinksFromFileCommand.php
+++ src/Domain/SiteBundle/Command/ImportAmazonAffiliateLinksFromFileCommand.php
@@ -18,6 +18,8 @@ class ImportAmazonAffiliateLinksFromFileCommand extends ContainerAwareCommand
 {
     private const CATEGORY_NAME_DELIMITER = '/';
 
+    private $output;
+
     protected function configure()
     {
         $this->setName('data:import-affiliate-links');
@@ -31,6 +33,7 @@ class ImportAmazonAffiliateLinksFromFileCommand extends ContainerAwareCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->output = $output;
         $path = $input->getArgument('path');
 
         if (!file_exists($path)) {
@@ -68,30 +71,31 @@ class ImportAmazonAffiliateLinksFromFileCommand extends ContainerAwareCommand
     {
         $em = $this->getContainer()->get('doctrine.orm.entity_manager');
         foreach ($data as $row) {
-            $categoryName = AdminHelper::convertAccentedString(trim(explode(self::CATEGORY_NAME_DELIMITER, $row['Category'])[0]));
+            $categoryName = trim(explode(self::CATEGORY_NAME_DELIMITER, $row['Category'])[0]);
             /** @var Category $category */
-            $category = $em->getRepository(Category::class)->getCategoryByCaseInsensitiveName([$categoryName]);
+            $category = $em->getRepository(Category::class)
+                ->getCategoryByCaseInsensitiveName([AdminHelper::convertAccentedString($categoryName)]);
 
-            if (!$category) {
-                throw new EntityNotFoundException('Category ' . $categoryName . ' not found');
+            if ($category) {
+                $url = new Url();
+                $url->setRelNoFollow(false);
+                $url->setRelNoOpener(false);
+                $url->setRelNoReferrer(false);
+                $url->setUrl($row['Amazon Affiliate Category Link']);
+                $category->setAmazonAffiliateUrl($url);
+
+                for ($i = 1; $i <= Category::MAX_AMAZON_AFFILIATE_ITEMS_COUNT; $i++) {
+                    $af = new AmazonAffiliateItem();
+                    $af->setCategory($category);
+                    $af->setEmbeddedHTML($row['Amazon Affiliate Link ' . $i]);
+                    $em->persist($af);
+                }
+
+                $em->flush();
+                $em->clear();
+            } else {
+                $this->output->writeln('Category "' . $categoryName . '" not found');
             }
-
-            $url = new Url();
-            $url->setRelNoFollow(false);
-            $url->setRelNoOpener(false);
-            $url->setRelNoReferrer(false);
-            $url->setUrl($row['Amazon Affiliate Category Link']);
-            $category->setAmazonAffiliateUrl($url);
-
-            for ($i = 1; $i <= Category::MAX_AMAZON_AFFILIATE_ITEMS_COUNT; $i++) {
-                $af = new AmazonAffiliateItem();
-                $af->setCategory($category);
-                $af->setEmbeddedHTML($row['Amazon Affiliate Link ' . $i]);
-                $em->persist($af);
-            }
-
-            $em->flush();
-            $em->clear();
         }
     }
 }
